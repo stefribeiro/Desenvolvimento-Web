deb03e20b039d1f9ffda284021113679d5aa38b4
stefane <stefaneribeiro@alu.ufc.br>
2022-06-12 23:49:54 -0300

    Lista4 finalizada

--- Lista4/script.js
+++ Lista4/script.js
@@ -1,9 +1,83 @@
-function autoResize()
+function redimensionar()
 {
-    textArea = document.getElementById('areaDeEntrada');
-    while (textArea.scrollHeight > textArea.offsetHeight)
+    textoArea = document.getElementById('areaDeEntrada');
+    while (textoArea.scrollHeight > textoArea.offsetHeight)
     {
-        textArea.rows += 1;
+        textoArea.rows += 1;
     }
+}
+
+function contar(){
+    let texto = document.getElementById('areaDeEntrada').value;
+    let divStatus = document.getElementById('statusEnvio'); 
+    let botao = document.getElementById('envio');
+
+    console.log(texto.length); 
+    
+    let h = document.createElement('h5');
+    h.innerHTML = "";
+    divStatus.innerHTML = "";
+    botao.disabled = true;
+
+    if(texto.length > 0)
+    {   
+        h.innerHTML = texto.length;
+        divStatus.innerHTML = "";
+        divStatus.appendChild(h);
+        divStatus.style.display = "block";
+        botao.disabled = false;
+
+        if(texto.length > 100)
+        {
+            h.style.color = "rgb(255, 200, 0)";
+            if(texto.length > 140) 
+            {
+                h.style.color = "rgb(255, 0, 0)";
+                botao.disabled = true;
+            }
+        }
+    }else{
+        h.innerHTML = "";
+        botao.disabled = true;
+    }
+
+
+}
+
+function contar2(){
+    let texto = document.getElementById('areaDeEntrada2').value;
+    let divStatus = document.getElementById('statusEnvio2'); 
+    let botao = document.getElementById('envio2');
+
+    console.log(texto.length); 
+    
+    let h = document.createElement('h5');
+    h.innerHTML = "";
+    divStatus.innerHTML = "";
+    botao.disabled = true;
+
+    if(texto.length > 0)
+    {   
+        h.innerHTML = texto.length;
+        divStatus.innerHTML = "";
+        divStatus.appendChild(h);
+        divStatus.style.display = "block";
+        botao.disabled = false;
+
+        if(texto.length > 100)
+        {
+            h.style.color = "rgb(255, 200, 0)";
+            if(texto.length > 140) 
+            {
+                h.style.color = "rgb(255, 0, 0)";
+                botao.disabled = true;
+            }
+        }
+    }else{
+        h.innerHTML = "";
+        botao.disabled = true;
+    }
+
+
 }
 
