eb73e958b0bb6ec90879e415e3ddce64d1d84ee5
stefane <stefaneribeiro@alu.ufc.br>
2022-07-19 17:39:18 -0300

    prova final

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h2>Tô Lendo</h2> 
+        <a href="\Login">
+            <button>Login</button>
+        </a>
+    </header>
+    <div id='leituras'>
+        <ul>
+        <h3>Biblioteca</h3>
+        <a class="livro" v-for="(livro, index) in livros" :key="index">
+            <img alt="livro.title" title="livro.title" src="livro.cover">
+        </a>
+    </div>
+</template>
+
+<script>
+import api from '../services/api.js'
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            livros: []
+        }
+    },
+    mounted(){
+        api.get('http://localhost:1337/api/books').then(response => {
+            this.livros = response.data;
+        })
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
